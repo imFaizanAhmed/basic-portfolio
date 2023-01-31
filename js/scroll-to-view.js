3309930e33ed038216f9ff678fa3cd4621229844
Faizan Ahmed <imfaizanahmed6@gmail.com>
2023-01-31 23:19:37 +0500

    path changed to journey

--- js/scroll-to-view.js
+++ js/scroll-to-view.js
@@ -1,8 +1,19 @@
 const projectButton = document.getElementById("project-button");
 const contactButton = document.getElementById("contact-button");
+const journeyButton = document.getElementById("journey-button");
+const scrollButton = document.getElementById("scroll-button");
 
 const projectSection = document.getElementById("projects");
 const contactSection = document.getElementById("contact");
+const timelineSection = document.getElementById("timeline");
+
+journeyButton.addEventListener("click", function () {
+  timelineSection.scrollIntoView({ behavior: "smooth" });
+});
+
+scrollButton.addEventListener("click", function () {
+  timelineSection.scrollIntoView({ behavior: "smooth" });
+});
 
 projectButton.addEventListener("click", function () {
   projectSection.scrollIntoView({ behavior: "smooth" });
