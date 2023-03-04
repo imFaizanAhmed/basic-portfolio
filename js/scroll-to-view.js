269409750130b0d041583617e435c316ca3b276f
Faizan Ahmed <imfaizanahmed6@gmail.com>
2023-03-04 15:22:23 +0500

    first draft is finalized

--- js/scroll-to-view.js
+++ js/scroll-to-view.js
@@ -1,32 +1,70 @@
-const aboutButton = document.getElementById("about-button");
-const projectButton = document.getElementById("project-button");
-const contactButton = document.getElementById("contact-button");
-const journeyButton = document.getElementById("journey-button");
+const aboutButton = document.getElementsByClassName("about-button");
+const projectButton = document.getElementsByClassName("project-button");
+const contactButton = document.getElementsByClassName("contact-button");
+const journeyButton = document.getElementsByClassName("journey-button");
 const scrollButton = document.getElementById("scroll-button");
+const mainDiv = document.getElementsByClassName("main")[0];
+const mobileMenu = document.getElementsByClassName("mobile-menu")[0];
+const menuToggle = document.getElementById("menuToggle");
+const menuToggleInput = document.getElementById("menuToggleInput");
 
 const introSection = document.getElementById("intro");
 const projectSection = document.getElementById("projects");
 const contactSection = document.getElementById("contact");
 const timelineSection = document.getElementById("timeline");
 
-aboutButton.addEventListener("click", function () {
-  introSection.scrollIntoView({ behavior: "smooth" });
-});
+for (let i = 0; i < aboutButton.length; i++) {
+  aboutButton[i].addEventListener("click", function () {
+    menuToggleInput.checked = false;
+    hiddeMobileMenu();
+    introSection.scrollIntoView({ behavior: "smooth" });
+  });
+}
 
-journeyButton.addEventListener("click", function () {
-  timelineSection.scrollIntoView({ behavior: "smooth" });
-});
+for (let i = 0; i < journeyButton.length; i++) {
+  journeyButton[i].addEventListener("click", function () {
+    menuToggleInput.checked = false;
+    hiddeMobileMenu();
+    timelineSection.scrollIntoView({ behavior: "smooth" });
+  });
+}
+
+for (let i = 0; i < contactButton.length; i++) {
+  contactButton[i].addEventListener("click", function () {
+    menuToggleInput.checked = false;
+    hiddeMobileMenu();
+    contactSection.scrollIntoView({ behavior: "smooth" });
+  });
+}
+
+for (let i = 0; i < projectButton.length; i++) {
+  projectButton[i].addEventListener("click", function () {
+    menuToggleInput.checked = false;
+    hiddeMobileMenu();
+    projectSection.scrollIntoView({ behavior: "smooth" });
+  });
+}
 
 scrollButton.addEventListener("click", function () {
   timelineSection.scrollIntoView({ behavior: "smooth" });
 });
 
-projectButton.addEventListener("click", function () {
-  projectSection.scrollIntoView({ behavior: "smooth" });
+menuToggle.addEventListener("click", function () {
+  if (mobileMenu.classList.contains("none")) {
+    mainDiv.style.display = "none";
+    mobileMenu.classList.remove("none");
+    mobileMenu.classList.add("flex");
+  } else {
+    hiddeMobileMenu();
+  }
 });
 
-contactButton.addEventListener("click", function () {
-  contactSection.scrollIntoView({ behavior: "smooth" });
-});
+function hiddeMobileMenu() {
+  if (mobileMenu.classList.contains("flex")) {
+    mainDiv.style.display = "block";
+    mobileMenu.classList.remove("flex");
+    mobileMenu.classList.add("none");
+  }
+}
 
 const directionRight = document.querySelector(".direction-r");
