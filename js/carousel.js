12bd712e1555f4c1e3995b3ffc2c7a0815d5339a
imFaizanAhmed <imfaizanahmed6@gmail.com>
2023-11-08 10:22:27 +0500

    carousel is working

---
+++ js/carousel.js
@@ -0,0 +1,28 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll('.carousel .slide');
+const totalSlides = slides.length;
+
+updateCarousel();
+let carouselTimer = 3000;
+
+document.getElementById('nextBtn').addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % totalSlides;
+    updateCarousel();
+});
+
+document.getElementById('prevBtn').addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+    updateCarousel();
+});
+
+function updateCarousel() {
+    slides.forEach((slide, index) => {
+        slide.style.transform = `translateX(${100 * (index - currentIndex)}%)`;
+    });
+}
+
+setInterval(() => {
+    console.log("hello");
+    currentIndex = (currentIndex + 1) % totalSlides;
+    updateCarousel();
+}, carouselTimer)
