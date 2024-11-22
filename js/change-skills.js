b01a91f5ab53c84e991631ae85be614cac5aef12
ImFaizanAhmed <imfaizanahmed6@gmail.com>
2024-11-23 00:19:51 +0500

    improv: update the experience and projects

--- js/change-skills.js
+++ js/change-skills.js
@@ -1,10 +1,12 @@
 const backendSkills = document.getElementById("backend-skills");
 const frontendSkills = document.getElementById("frontend-skills");
 const toolsSkills = document.getElementById("tools-skills");
+const devOpsSkills = document.getElementById("DevOps-skills");
 const apisSkills = document.getElementById("apis-skills");
 
 const backend = document.getElementById("backend");
 const frontend = document.getElementById("frontend");
+const devOps = document.getElementById("devOps");
 const tools = document.getElementById("tools");
 const apis = document.getElementById("apis");
 
@@ -13,23 +15,35 @@ backend.addEventListener("click", function () {
   backendSkills.classList.add("skills");
   frontendSkills.classList.remove("skills");
   toolsSkills.classList.remove("skills");
+  devOpsSkills.classList.remove("skills");
   apisSkills.classList.remove("skills");
 });
 frontend.addEventListener("click", function () {
   frontendSkills.classList.add("skills");
   backendSkills.classList.remove("skills");
   toolsSkills.classList.remove("skills");
+  devOpsSkills.classList.remove("skills");
+  apisSkills.classList.remove("skills");
+});
+devOps.addEventListener("click", function () {
+  toolsSkills.classList.remove("skills");
+  backendSkills.classList.remove("skills");
+  frontendSkills.classList.remove("skills");
+  devOpsSkills.classList.add("skills");
   apisSkills.classList.remove("skills");
 });
 tools.addEventListener("click", function () {
+  console.log("tools");
   toolsSkills.classList.add("skills");
   backendSkills.classList.remove("skills");
   frontendSkills.classList.remove("skills");
+  devOpsSkills.classList.remove("skills");
   apisSkills.classList.remove("skills");
 });
 apis.addEventListener("click", function () {
   apisSkills.classList.add("skills");
   backendSkills.classList.remove("skills");
   frontendSkills.classList.remove("skills");
+  devOpsSkills.classList.remove("skills");
   toolsSkills.classList.remove("skills");
 });
